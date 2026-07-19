a488896abbf59a542779608850cd5db5229a9125
Medhat31 <medhatkhalil340@gmail.com>
2026-07-20 02:38:38 +0300

    feat: added logic for emulator cli and input validation

--- emulator/src/main/java/com/snmp/emulator/Main.java
+++ emulator/src/main/java/com/snmp/emulator/Main.java
@@ -5,5 +5,19 @@ public class Main {
 
     public static void main(String[] args) {
         
+         if (args.length == 0) {
+             System.out.println("Error: This emulator requires arguments");
+         }
+        
+        if (args.length != 5) {
+            System.out.println("Invalid arguments");
+            System.out.println("Expected arguments : "
+                    + "<OperationMode(--cli or --gui)> <NodeType> <TargetIP> <TargetPort>");
+            return;
+        }
+        
+        if ("--cli".equals(args[0]) || "cli".equals(args[0])) {
+            EmulatorCLI.run(args);
+        }
     }
 }
