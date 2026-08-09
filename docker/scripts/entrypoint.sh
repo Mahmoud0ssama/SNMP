c43730acb157c5ad5c563bb95e3a8f78e3ab964f
Mahmoud Osama <mahmoud02505@gmail.com>
2026-08-09 16:07:27 +0300

    chore: clean up  unused files

--- docker/scripts/entrypoint.sh
+++ docker/scripts/entrypoint.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# Start the heartbeat daemon in the background
-/usr/local/bin/start_heartbeat.sh
-
 # Start the fault monitoring daemon in the background
 /usr/local/bin/fault_monitor_daemon.sh &
 
